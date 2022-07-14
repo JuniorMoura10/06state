cd3f6aac79301ecbee04a91295f7715132a19a57
Júnior Moura <geraldomartins10@gmail.com>
2022-07-14 17:22:54 -0300

    events

--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Componente extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {nome: 'Júnior'};
+    
+    mensagem(nome){
+        alert('Hello, ' + nome);
+        console.log(this);
+    }
+
+    minhaArrouFunction = (curso) => {
+        alert('Eu estou fazendo o curso de ' + curso);
+    }
+
+    teclado = (obj) => {
+        console.log(obj.target.value);
     }
 
     render() {
-        return <h1>{this.state.nome}</h1>
+        return(
+            <div>
+                <button onClick={this.mensagem.bind(this, 'Júnior Moura!')}>Clique aqui</button>
+                <button onClick={() => this.minhaArrouFunction('ReactJS')}>Arrow Function</button>
+                <hr/>
+
+                <input type="password" onChange={this.teclado} />
+
+            </div>
+        ); 
     }
 }
 
